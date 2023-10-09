ee126039926087291da2eab76dcc3949d160db32
roxxe34 <hamzafarissi7@gmail.com>
2023-10-09 15:59:19 +0100

    done

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k in ("created_at", "updated_at"):
+                    self.__dict__[k] = datetime.strptime(v, date_format)
+                else:
+                    self.__dict__[k] = v
+
+
+
+
+    def save(self):
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
+
+
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+
+
